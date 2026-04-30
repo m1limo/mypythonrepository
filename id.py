9eb71c2bf69f70d5e88e67b83ae1d2171a43c230
m1limo <milimofranklin20@gmail.com>
2026-04-29 18:32:54 -0700

    session3

---
+++ id.py
@@ -0,0 +1,16 @@
+
+#ID_2 = int(input("Enter ID: "))
+#print(type(ID_2))#
+
+"""name = input("Enter name: ")#
+print(type(name))
+# lists
+fruts = ["apples", "bananas"]
+print(fruts)
+
+##tuple#
+fruits = ("mango", "banana")
+"""
+"""#sets
+fruits = {"banana", "apple"}
+"""

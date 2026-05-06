d3affc605a140e591e184dc481db2303512bfa2e
m1limo <milimofranklin20@gmail.com>
2026-05-06 18:27:03 +0300

    session_4

--- id.py
+++ id.py
@@ -14,3 +14,77 @@ fruits = ("mango", "banana")
 """#sets
 fruits = {"banana", "apple"}
 """
+
+#LISTS
+"""#track land parcel IDs
+
+parcels = [ 'p001', 'p002'] #separate land parcels with a comma
+parcels.append('p003')   #add a new parcel ID to the list
+print(parcels)
+parcels.insert(1, 'p00l B') #insert item at index 1
+print(parcels)
+parcels.remove('p002') #remove item
+print(parcels)
+parcels.sort()
+
+##Methods are .append .reverse, .
+print(len(parcels))
+"""
+#TUPLES
+"""
+capital_cities = ("Nairobi", "Addis Ababa", "Dar es Salaam", "Kampala", "Kigali")
+print(len(capital_cities))
+print(capital_cities[0])
+capital_cities.append("Kisumu")
+"""
+
+
+#SETS
+"""survey_A = {"Nairobi", "Mombasa", "Kisumu"}
+survey_B = {"Kisumu", "Nakuru"}
+
+#union - all covered counties
+print(survey_A | survey_B) 
+print(survey_A & survey_B) #intersection - common counties 
+print(survey_A - survey_B) #difference - counties in A but not in B
+
+survey_A.add("Nairobi")
+print(survey_A)
+ """
+ 
+#DICRIONARIES
+"""student = {"name": "Franklin",
+           "age": 20,
+           "Nationality" : "Kenyan"
+           }
+print(student["name"])
+
+student_2 = {
+    
+}
+name_2 = input("Enter name: ")
+student_2["name"] = name_2
+
+print(student_2)
+#.keys()  
+print(student.keys())
+#.values() 
+print(student_2.values())
+"""
+#INDEXING/SLICING
+"""fruits = ["apples", "banana", "mango", "pineapple"]
+print(fruits[0])
+#SLICING
+#list(start:stop:step)
+print(fruits[0:2])#first value inclusive last value exclusive
+print(fruits[0:-1]) #reversed
+print(fruits[0:]) #from index 0 to the end of the list
+print(fruits[:3]) #from the beginning to index 3 exclusive
+print(fruits[::2]) #steps up 2 in the whole list
+print(fruits[-1: ])
+
+"""
+
+
+
+
